786ed464f976477a8615719b75ccef78b3a47c37
Kutluhan Türkmen <mustafademirtra@gmail.com>
2025-08-16 13:11:11 +0300

    Create general.py

---
+++ cogs/general.py
@@ -0,0 +1,12 @@
+from discord.ext import commands
+
+class General(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f"Pong! {round(self.bot.latency*1000)}ms")
+
+def setup(bot):
+    bot.add_cog(General(bot))
